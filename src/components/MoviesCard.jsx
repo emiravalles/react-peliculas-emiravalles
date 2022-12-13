b589922e180c1adc53f2650a6a5f46aef98ed772
emiravalles <emanuelmiravalles@gmail.com>
2022-12-13 01:05:42 -0300

    Algunos cambios estéticos y agregado de fondo

--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -4,16 +4,28 @@ import "./MoviesCard.css";
 
 const MySwal = withReactContent(Swal);
 
-const showDescription = (title, description, image) => {
+const showDescription = (title, description, image, id) => {
     MySwal.fire({
         title: title,
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        },
         text: description,
         imageUrl: image,
         imageWidth: 200,
         imageHeight: 300,
         imageAlt: title,
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'Close'
+        confirmButtonColor: '#000080',
+        // confirmButtonText: '+ More info',
+        confirmButtonText: '<a class="more_info" href="./secondpage.php">+ More info</a>',
+        // footer: '<a href="./secondpage.php">+ More info</a>',
+        showCancelButton: true,
+        cancelButtonText: 'Close',
+        reverseButtons: true,
+        focusConfirm: false
     })
 }
 
@@ -21,7 +33,7 @@ export function MoviesCard ({movie}) {
     const imgUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
     return (
         <li className="movieCard">
-            <img className="movieImage" src={imgUrl} alt={movie.title} onClick={()=>{showDescription(movie.title, movie.overview, imgUrl)}} />
+            <img className="movieImage" src={imgUrl} alt={movie.title} onClick={()=>{showDescription(movie.title, movie.overview, imgUrl, movie.id)}} />
             <div className="movieTitle">{movie.title}</div>
         </li>
     );
